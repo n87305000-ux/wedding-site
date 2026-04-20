112ed6f519d6dbde90bf1d01ad650ecf601538e9
n87305000-ux <n87305000@gmail.com>
2026-04-20 08:40:20 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function start() {
+  document.querySelector(".hero").style.display = "none";
+  document.getElementById("content").style.display = "block";
+}
+
+function celebrate() {
+  alert("🎉 Congratulations! 🎉");
+}
